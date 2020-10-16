5c2ce93ec1e25ea50451f76c9cc2974183efe97a
Prince Kumar Singh <monukr377@gmail.com>
2020-10-16 12:35:30 +0530

    changesAdded

--- src/index.js
+++ src/index.js
@@ -14,6 +14,108 @@ app.use(bodyParser.json())
 // your code goes here
 
 // here
+app.get('/', (req,res)=>{
+    return res.status(200).message("Hello world");
+});
+
+app.post("/add",function(req,res){
+            
+    // console.log(req.body);
+        let num1=parseFloat(req.body.v1);
+        let num2=parseFloat(req.body.v2);
+        if(isNaN(num1)||isNaN(num2)){
+         return  res.json({
+             status:"error",
+             message:"Invalid data types",
+         })
+     }
+     if(num1>1000000||num2>1000000){
+         return res.status(200).json({
+             'message': "overflow"
+         })
+     }
+     const sum = num1 + num2;
+     return res.status(200).json({
+         status:"success",
+         'message': 'the sum of given two numbers',
+         'sum': sum
+     })
+}); 
+
+
+
+
+
+app.post("/sub",function(req,res){
+            
+        let num1=parseFloat(req.body.v1);
+        let num2=parseFloat(req.body.v2);
+        if(isNaN(num1)||isNaN(num2)){
+         return  res.json({
+             status:"error",
+             message:"Invalid data types",
+         })
+     }
+     if(num1<-1000000||num2<-1000000){
+         return res.status(200).json({
+             'message': "underflow"
+         })
+     }
+     const sub = num1 - num2;
+     return res.status(200).json({
+         status:"success",
+         'message': 'the substract of given two numbers',
+         'sub': sub
+     })
+}); 
+
+app.post("/multiply",function(req,res){
+            
+        let num1=parseFloat(req.body.v1);
+        let num2=parseFloat(req.body.v2);
+        if(isNaN(num1)||isNaN(num2)){
+         return  res.json({
+             status:"error",
+             message:"Invalid data types",
+         })
+     }
+     if(num1>1000000||num2>1000000){
+         return res.status(200).json({
+             'message': "overflow"
+         })
+     }
+     const mult = num1 * num2;
+     return res.status(200).json({
+         status:"success",
+         'message': 'The product of given numbers',
+         'mult': mult
+     })
+}); 
+
+app.post("/calculator",function(req,res){
+            
+
+        let num1=parseFloat(req.body.v1);
+        let num2=parseFloat(req.body.v2);
+        if(isNaN(num1)||isNaN(num2)){
+         return  res.json({
+             status:"error",
+             message:"Invalid data types",
+         })
+     }
+     if(num2==0){
+         return res.status(200).json({
+             'message': "Cannot divide by Zero"
+         })
+     }
+     const div = num1 / num2;
+     return res.status(200).json({
+         status:"success",
+         'message': 'The divison of given numbers',
+         'div': div
+     })
+}); 
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
